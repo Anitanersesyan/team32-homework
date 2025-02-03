dc06dc814ee84287e246ed0ac8a659dd3bae4ca2
anitanersesyan <anita140701@gmail.com>
2025-02-03 19:47:21 +0100

    Add Week 1 files: Age-ify.js, Ez-Namey.js, Goodboy-Oldboy.js, Housey-pricey.js, index.html

---
+++ javascript/javascript1/week1/Ez-Namey.js
@@ -0,0 +1,19 @@
+/* Create two arrays called firstWords, secondWords. The arrays should have 10 elements containing strings of possible startup names. Some examples could be: "Easy", "Awesome", "Corporate".
+
+Create a variable called startupName that will contain the created startup name.
+
+Using a random index (you choose) of the arrays and concatenation of strings, create and log the new startup name and the number of characters in it. An example could be: "The startup: "Easy Corporation" contains 16 characters"
+
+Hint: you can use this code to generate a random number from 0-9, if you dont want to specify the indexes yourself.
+
+const randomNumber = Math.floor(Math.random() * 10);*/
+
+const firstWords = ["Easy", "Awesome", "Corporate", "Innovative","Tech", "Smart", "Dynamic", "Global", "Creative", "NextGen"];
+const secondWords = ["Corporation", "Startup", "Company",   "Technologies", "Ventures", 
+"Inc", "Works", "Group", "Enterprises", "Partners"];
+
+const randomNumber = Math.floor(Math.random() * 10);
+let startupName = `${firstWords[randomNumber]} ${secondWords[randomNumber]}`;
+
+console.log(`The startup: ${startupName} contains ${startupName.length} characters`);
+
---
+++ javascript/javascript1/week1/Housey-pricey.js
@@ -0,0 +1,19 @@
+
+/* We have made our own formula for calculating the price of a house. This formula is VERY simplified and not at all close to the real calculation which can get quite complicated:
+
+housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+Your friend Peter is considering a house that is 8m wide, 10m deep and 10m high. The garden size is 100m2. The house costs 2.500.000. Your friend Julia is considering a house that is 5m wide, 11m deep and 8m high. The garden size is 70m2. This house costs 1.000.000.
+
+Figure out if Peter and Julia are paying too much or too little using Javascript and the formula specified above. */
+
+
+let peterHouse = 8 * 10 * 10 * 2.5 * 1000 + 100 * 300;
+let juliaHouse = 5 * 11 * 8 * 2.5 * 1000 + 70 * 300;
+
+if (peterHouse > juliaHouse) {
+    console.log("Peter is paying too much");
+} else if (peterHouse < juliaHouse) {
+    console.log("Julia is paying too much");
+} else {
+    console.log("Both are paying the same amount");
+}
